f4bb12db4dcec442148565b7dc03f1e8181b8252
Ahmed hanzala <134609073+hanzalagithub@users.noreply.github.com>
2024-03-02 14:42:58 +0500

    02

--- App.tsx
+++ App.tsx
@@ -1,21 +1,52 @@
-import { View, Text, SafeAreaView, ScrollView } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, ScrollView, StatusBar, TouchableOpacity, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
 import FlashCards from './Components/FlashCards'
 import ElevatedCards from './Components/ElevatedCards'
 import FancyCards from './Components/FancyCards'
 import ActionCard from './Components/ActionCard'
 
 const App = () => {
+  const [randomBackground, setrandomBackground] = useState('#ffffff');
+  const generateColor = () => {
+    const hexRange = '0123456789ABCDEF'
+    let color = '#'
+    for (let i = 0; i < 6; i++){
+      color += hexRange[Math.floor(Math.random()*16)]
+    }
+    setrandomBackground(color) 
+  }
   return (
     <SafeAreaView>
       <ScrollView>
+        <StatusBar backgroundColor={randomBackground}/>
         <FlashCards />
         <ElevatedCards />
         <FancyCards />
         <ActionCard />
+        <View style={[styles.container,{backgroundColor: randomBackground}]}>
+          <TouchableOpacity onPress={generateColor}>
+            <View style={styles.actionBtn}>
+              <Text>press me</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     </SafeAreaView>
   )
 }
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  actionBtn:{
+    borderRadius:12,
+    backgroundColor:'blue',
+    paddingVertical:10,
+    paddingHorizontal:40
+  }
+  
+})
 
 export default App
